f803e5b862430b36d110940b4a6367aa403ab5d4
trevanian66 <kwablah@hotmail.com>
2026-09-07 15:42:48 +0100

    initial build

--- ShowsecPoll/ContentView.swift
+++ ShowsecPoll/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pollManager = PollingManager()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            Text("Latest Server Status").font(.headline)
+            Text(pollManager.receiveddata).font(.body)
         }
         .padding()
+        .onAppear {
+            pollManager.startPolling()
+        }
+        .onDisappear {
+            pollManager.stopPolling()
+        }
     }
 }
 
--- ShowsecPoll/Models/PolliingManager.swift
+++ ShowsecPoll/Models/PolliingManager.swift
@@ -5,4 +5,96 @@
 //  Created by Kobby Awadzi on 07/09/2026.
 //
 
-import Foundation
+import SwiftUI
+
+@Observable
+class PollingManager  {
+    private var isPolling: Bool = false
+    let sleepTimeInSeconds = 10.0
+    var receiveddata: String = "loading..."
+    let boundary = "-----------B4EBB869-A29D-4DA3-B962-6F23FE749076"
+    let username = "KwablahAwadzi"
+    let password = "yyAuTS6LLQ2ofXRUZxUnTCPc06EEb+Q7TJDcujU89bA="
+    
+    
+    func startPolling() {
+        guard !isPolling else { return }
+        isPolling = true
+        
+        Task {
+            
+          await login()
+            
+        }
+    }
+    
+    func stopPolling() {
+        isPolling = false
+    }
+    
+     func login() async {
+         let loginURLString = "https://publicapi.smartg8.com/Authentication/JWTLogin"
+         let initialBoundary = Data("\(boundary)\r\n".utf8)
+         let intermediaryBoundary = Data("\r\n\(boundary)\r\n".utf8)
+         let closingBoundary = Data("\r\n\(boundary)--\r\n".utf8)
+         let initialBoundaryString = "\(boundary)"
+        
+         
+        
+         var request = URLRequest(url: URL(string: loginURLString)!)
+         request.httpMethod = "POST"
+         
+        // request.setValue("publicapi.smartg8.com", forHTTPHeaderField: "Host")
+         request.setValue("application/json,text/json,text/x-json,text/javascript,application/xml,text/xml", forHTTPHeaderField: "Accept")
+         request.setValue("showsec", forHTTPHeaderField: "Company")
+         request.setValue("RestSharp/106.12.0.0", forHTTPHeaderField: "User-Agent")
+         request.setValue("Keep-Alive", forHTTPHeaderField: "Connection")
+         request.setValue("gzip, deflate", forHTTPHeaderField: "Accept-Encoding")
+         request.setValue("multipart/form-data; boundary=\(initialBoundaryString)", forHTTPHeaderField: "Content-Type")
+   
+        
+         var data = Data()
+         data.append(initialBoundary)
+         data.append(Data("Content-Disposition: form-data; name=\"username\"\r\n\r\n".utf8))
+         data.append(username.data(using: .utf8)!)
+         data.append(intermediaryBoundary)
+         data.append(Data("Content-Disposition: form-data; name=\"password\"\r\n\r\n".utf8))
+         data.append(password.data(using: .utf8)!)
+         data.append(closingBoundary)
+         request.setValue(data.count.description, forHTTPHeaderField: "Content-Length")
+       //  request.httpBody = data
+        
+         request.debugPrint()
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse else { return }
+            print("status code: \(httpResponse.statusCode)")
+            
+            if let data = data {
+                self.receiveddata =  String(data: data, encoding: .utf8) ?? "no data"
+                print("DATA IS: \(self.receiveddata)")
+            }
+        }
+        task.resume()
+        
+    }
+    
+    private func fetchNetowrkData() {
+        
+    }
+    
+}
+
+fileprivate extension URLRequest {
+    func debugPrint() {
+        print("\(self.httpMethod ?? "") \(self.url?.absoluteString ?? "")")
+        print("headers:")
+        print(self.allHTTPHeaderFields!)
+        print("body:")
+        print(String(data: self.httpBody ?? Data(), encoding: .utf8)!)
+    }
+    
+}
