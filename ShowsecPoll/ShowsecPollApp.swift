693af85bd61895305fff344b61ce9d75c1899dd0
trevanian66 <kwablah@hotmail.com>
2026-09-07 13:08:18 +0100

    Initial Commit

---
+++ ShowsecPoll/ShowsecPollApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowsecPollApp.swift
+//  ShowsecPoll
+//
+//  Created by Kobby Awadzi on 07/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShowsecPollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShowsecPollTests/ShowsecPollTests.swift
@@ -0,0 +1,19 @@
+//
+//  ShowsecPollTests.swift
+//  ShowsecPollTests
+//
+//  Created by Kobby Awadzi on 07/09/2026.
+//
+
+import Testing
+@testable import ShowsecPoll
+
+struct ShowsecPollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
