edd96cfa8f1356c4ed4ba988fdfa4a2362178090
trevanian66 <kwablah@hotmail.com>
2026-09-07 16:58:46 +0100

    first working version!

--- ShowsecPoll/Models/PolliingManager.swift
+++ ShowsecPoll/Models/PolliingManager.swift
@@ -12,7 +12,7 @@ class PollingManager  {
     private var isPolling: Bool = false
     let sleepTimeInSeconds = 10.0
     var receiveddata: String = "loading..."
-    let boundary = "-----------B4EBB869-A29D-4DA3-B962-6F23FE749076"
+    let boundary = "---------B4EBB869-A29D-4DA3-B962-6F23FE749076"
     let username = "KwablahAwadzi"
     let password = "yyAuTS6LLQ2ofXRUZxUnTCPc06EEb+Q7TJDcujU89bA="
     
@@ -34,9 +34,9 @@ class PollingManager  {
     
      func login() async {
          let loginURLString = "https://publicapi.smartg8.com/Authentication/JWTLogin"
-         let initialBoundary = Data("\(boundary)\r\n".utf8)
-         let intermediaryBoundary = Data("\r\n\(boundary)\r\n".utf8)
-         let closingBoundary = Data("\r\n\(boundary)--\r\n".utf8)
+         let initialBoundary = Data("--\(boundary)\r\n".utf8)
+         let intermediaryBoundary = Data("\r\n--\(boundary)\r\n".utf8)
+         let closingBoundary = Data("\r\n--\(boundary)--\r\n".utf8)
          let initialBoundaryString = "\(boundary)"
         
          
@@ -44,7 +44,7 @@ class PollingManager  {
          var request = URLRequest(url: URL(string: loginURLString)!)
          request.httpMethod = "POST"
          
-        // request.setValue("publicapi.smartg8.com", forHTTPHeaderField: "Host")
+         request.setValue("publicapi.smartg8.com", forHTTPHeaderField: "Host")
          request.setValue("application/json,text/json,text/x-json,text/javascript,application/xml,text/xml", forHTTPHeaderField: "Accept")
          request.setValue("showsec", forHTTPHeaderField: "Company")
          request.setValue("RestSharp/106.12.0.0", forHTTPHeaderField: "User-Agent")
